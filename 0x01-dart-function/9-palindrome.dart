3f7a5285e26d416665e93443fcb9e61dc8c03609
jeanpierreba <2713@holbertonschool.com>
2022-07-19 14:04:14 -0500

    Added task 9

---
+++ 0x01-dart-function/9-palindrome.dart
@@ -0,0 +1,13 @@
+bool isPalindrome(String s) {
+  if (s.length < 3 )
+    return false;
+  String reverse = "";
+
+  for (var i = s.length - 1; i >= 0, i--) {
+	reverse += s[i];
+  }
+  if (reverse = s)
+    return true;
+  else
+    return false;
+}
