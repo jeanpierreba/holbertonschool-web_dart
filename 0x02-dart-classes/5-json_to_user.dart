f421ccfe3d6192e15dcb2fd202a19218dbe3fbed
jeanpierreba <2713@holbertonschool.com>
2022-07-21 15:02:15 -0500

    Changed code

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -1,31 +1,28 @@
-import "dart:io";
-
 class User {
-  User(
-	{int? this.id,
-	required this.name,
-	required this.age,
-	required this.height});
+  int? id;
   String name;
   int age;
   double height;
-  int? id;
+
+  User(
+      {int? this.id,
+      required this.name,
+      required this.age,
+      required this.height});
 
   Map toJson() {
-	return {"id": id, "name": name, "age": age, "height": height};
+    return {"id": id, "name": name, "age": age, "height": height};
   }
 
   static User fromJson(Map<dynamic, dynamic> userJson) {
-	return User(
-	  id: userJson['id'],
-	  name: userJson['name'],
-	  age: userJson['age'],
-	  height: userJson['height']);
+    final userId = userJson['id'] as int;
+    final userName = userJson['name'] as String;
+    final userAge = userJson['age'] as int;
+    final userHeight = userJson['height'] as double;
+    return User(id: userId, name: userName, age: userAge, height: userHeight);
   }
 
   @override
-  String toString() {
-    var message = 'User(id : $id ,name: $name, age: $age, height: $height)';
-	return message;
-  }
+  String toString() =>
+      'User(id : $id ,name: $name,  age: $age, height: $height)';
 }
