08199a5944815489a10d6c15a417bfeda32629fa
jeanpierreba <2713@holbertonschool.com>
2022-07-21 15:09:30 -0500

    Reverted changes

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -1,30 +1,28 @@
 class User {
   User(
-      {required this.id,
-      required this.name,
-      required this.age,
-      required this.height});
+	{int? this.id,
+	required this.name,
+	required this.age,
+	required this.height});
   String name;
-  int id;
   int age;
   double height;
+  int? id;
+
   Map toJson() {
-    return {"id": id, "name": name, "age": age, "height": height};
+	return {"id": id, "name": name, "age": age, "height": height};
   }
 
   static User fromJson(Map<dynamic, dynamic> userJson) {
-    return User(
-        id: userJson['id'],
-        name: userJson['name'],
-        age: userJson['age'],
-        height: userJson['height']);
+	return User(
+	  id: userJson['id'],
+	  name: userJson['name'],
+	  age: userJson['age'],
+	  height: userJson['height']);
   }
 
-  String showName() {
-    return "Hello $name";
-  }
-
+  @override
   String toString() {
-    return "User(id : $id ,name: $name, age: $age, height: $height)";
+	return 'User(id : $id ,name: $name, age: $age, height: $height)';
   }
 }
